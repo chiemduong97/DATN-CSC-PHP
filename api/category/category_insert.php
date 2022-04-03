02f3adf75c41530fec9e7a392807244b9603a76b
Vuong Tran <tranvuongdouble@gmail.com>
2022-04-03 17:25:56 +0700

    finish api category

---
+++ api/category/category_insert.php
@@ -0,0 +1,22 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/configHeader.php'; 
+include_once $_SERVER['DOCUMENT_ROOT'].'/controllers/category/category_controller.php';
+
+    (new CF_Header()) -> config("POST");
+
+    $body = json_decode(file_get_contents("php://input"));
+
+    if(property_exists($body, 'name') && property_exists($body, 'avatar')){
+        $name = $body->name;
+        $avatar = $body->avatar;
+        $data = (new CategoryController()) -> insertItem($name, $avatar);
+    
+        if($data){
+            echo json_encode($data);
+        }else{
+
+        }
+    }
+
+    
+?>
