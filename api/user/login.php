e63d8445eb1acb6054aa3a5c87e4618366fc7c81
Vuong Tran <tranvuongdouble@gmail.com>
2022-06-07 17:55:28 +0700

    update login

--- api/user/login.php
+++ api/user/login.php
@@ -8,6 +8,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/libs/php-jwt-master/src/BeforeValidEx
 include_once $_SERVER['DOCUMENT_ROOT'] . '/libs/php-jwt-master/src/ExpiredException.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/libs/php-jwt-master/src/SignatureInvalidException.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/libs/php-jwt-master/src/JWT.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/models/response_model.php';
 
 (new CF_Header())->config("POST");
 $code = 1001;
@@ -29,22 +30,21 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     $data = (new UserController())->login($email, $password);
 
     if ($data == 1000) {
-        echo json_encode(array(
-            "code" => 1000,
+        $code = 1000;
+        $data = array(
             "accessToken" => $jwt
-        ));
+        );
     } else {
-        echo json_encode(array(
-            "isError" => true,
-            "code" => $data,
-            "message" => "Login failed !"
-        ));
+        $code =  $data;
     }
 } else {
-    echo json_encode(array(
-        "isError" => true,
-        "code" => 1013,
-        "message" => "Please fill out completely !"
-    ));
+    $code = 1013;
 }
+
+echo (
+    (new Response(
+        $code,
+        $data,
+    ))->response()
+);
 
