1429c934150c70159e5fbdbd56240c325ff280f1
DOCHIEMDUONG <chiemduong.dp.cntt@gmail.com>
2022-07-10 19:25:21 +0700

    update

---
+++ config/nuoc_ngot_chai.sql
@@ -0,0 +1,94 @@
+insert into `products` ( `name`, `avatar`,`description`,`price`,`category_id`) values 
+
+( 
+'Nước Ngọt Coca Cola Chai Pet 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-cola-chai-pet-390ml-1657448315?w=960&type=s',
+'Từ thương hiệu nước giải khát hàng đầu thế giới, nước ngọt Coca Cola chai pet 390ml xua tan nhanh mọi cảm giác mệt mỏi, căng thẳng, đặc biệt thích hợp sử dụng với các hoạt động ngoài trời. Bên cạnh đó thiết kế dạng chai nhỏ gọn, tiện lợi dễ dàng bảo quản khi không sử dụng hết',
+10000, 15
+),
+( 
+'Nước Ngọt Sprite Pet 1,85l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-sprite-pet-1-85l-1654674645?w=960&type=s',
+'Hương vị được ưa chuộng tại hơn 190 quôc gia và lọt top những nước giải khát được yêu thích nhất toàn cầu. Với vị chanh tươi mát cùng những bọt ga sảng khoái tê đầu lưỡi giúp bạn đập tan cơn khát ngay tức thì. Sản phẩm cam kết chính hãng, chất lượng và an toàn từ nhà Sprite
+Thương hiệu: Sprite (Việt Nam)',
+21000, 15
+),
+( 
+'Sá Xị Chương Dương Chai 1.5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-sa-xi-chuong-duong-chai-1-5l-1656646633?w=960&type=s',
+'Sá xị là sản phẩm nước giải khát truyền thống của Chương Dương và là sản phẩm mang hương vị độc đáo của Việt Nam, được khách hàng rất ưa chuộng. Sản phẩm có thành phần quế và hồi hỗ trợ tiêu hóa, hô hấp, tuần hoàn, làm ấm cơ thể.
+Ngon hơn khi uống lạnh',
+23000, 15
+),
+( 
+'Coca Zero 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-coca-zero-390ml-1654184890?w=960&type=s',
+'Từ thương hiệu nước giải khát Coca Cola nổi tiếng thế giới được ưa chuộng tại nhiều nhiều quốc gia. Coca Zero 390ml cho cơ thể cảm giác nhẹ nhàng, ăn ngon hơn, không đường không calo phù hợp cho những ai yêu thích nước ngọt nhưng vẫn muốn giữ thói quen ăn uống lành mạnh
+Thương hiệu: Coca Cola (Mỹ)',
+9000, 15
+),
+( 
+'Nước Ngọt Coca Ít Đường 1,5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-it-duong-1-5l-1654681363?w=960&type=s',
+'Không chỉ giúp giải tỏa cơn khát mà còn cung cấp nguồn năng lượng cùng hàm lượng khoáng chất dồi dào, cho bạn khơi lại hứng khởi ngay từ ngụm đầu tiên.Nước Ngọt Coca Ít Đường 1,5l phù hợp cho những ai thích nước ngọt và lối sống ít đường, lành mạnh. Cam kết chính hãng từ Coca Cola
+Thương hiệu: Coca Cola (Mỹ)',
+22000, 15
+),
+( 
+'Nước Ngọt Coca Ít Đường 2,25l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-it-duong-2-25l-1654681379?w=960&type=s',
+'Từ thương hiệu loại nước giải khát Coca Cola được nhiều người yêu thích với hương vị thơm ngon, sảng khoái. Nước ngọt Coca Cola Ít Đường chai 2.25 lít với lượng gas lớn sẽ giúp bạn xua tan mọi cảm giác mệt mỏi, căng thẳng, đem lại cảm giác thoải mái sau khi hoạt động ngoài trời.',
+28000, 15
+),
+( 
+'Nước Ngọt Coca Không Đường 1,5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-khong-duong-1-5l-1654184857?w=960&type=s',
+'Từ thương hiệu nước giải khát Coca Cola nổi tiếng thế giới được ưa chuộng tại nhiều nhiều quốc gia. Nước Ngọt Coca Không Đường 1,5l cho cơ thể cảm giác nhẹ nhàng, ăn ngon hơn, không đường không calo phù hợp cho những ai yêu thích nước ngọt nhưng vẫn muốn giữ thói quen ăn uống lành mạnh
+Thương hiệu: Coca Cola (Mỹ)',
+20000, 15
+),
+( 
+'Nước Ngọt Fanta Cam Chai 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-fanta-cam-chai-390ml-1654185077?w=960&type=s',
+'Là sản phẩm nước ngọt có gas của thương hiệu Fanta nổi tiếng giúp giải khát sau khi hoạt động ngoài trời, giải tỏa căng thẳng, mệt mỏi khi học tập, làm việc. Nước Ngọt Fanta Cam Chai 390ml thơm ngon kích thích vị giác, chứa nhiều vitamin C sẽ cung cấp năng lượng cho cơ thể khỏe mạnh',
+8000, 15
+),
+(
+'Nước Ngọt Mirinda Cam 1.5 Lít - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-mirinda-cam-1-5-lit-1654161021?w=960&type=s',
+'Sản phẩm nước ngọt giải khát từ thương hiệu Mirinda nổi tiếng được nhiều người ưa chuộng. Nước ngọt Mirinda cam 1.5 lít với hương vị cam đặc trưng, không chỉ giải khát, mà còn bổ sung thêm vitamin C giúp lấy lại năng lượng cho hoạt động hàng ngày. Cam kết chính hãng và an toàn
+Hướng dẫn sử dụng: Dùng trực tiếp, ngon hơn khi uống lạnh.
+Bảo quản: Để nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp.',
+22000, 15
+),
+(
+'Nước Ngọt Mirinda Xá Xị 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-mirinda-xa-xi-390ml-1653557418?w=960&type=s',
+'Sản phẩm nước ngọt giải khát từ thương hiệu Mirinda nổi tiếng được nhiều người ưa chuộng với hương và vị hấp dẫn. Nước ngọt Mirinda hương xá xị chai 390ml có hương xá xị tự nhiên, độc đáo giúp bạn giải nhanh cơn khát,  với vị gas nhẹ là thức uống giải khát tuyệt vời dành cho mọi lứa tuổi',
+8000, 15
+),
+(
+'Nước Ngọt Pepsi 1.5 Lít - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-pepsi-1-5-lit-1653557377?w=960&type=s',
+'Là loại nước ngọt được nhiều người yêu thích với hương vị thơm ngon, sảng khoái. Nước ngot Pepsi 1.5 lít với lượng gas lớn sẽ giúp bạn xua tan mọi cảm giác mệt mỏi, căng thẳng, đem lại cảm giác thoải mái sau khi hoạt động ngoài trời.',
+21000, 15
+),
+(
+'Nước Ngọt Sprite Hương Chanh 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-sprite-huong-chanh-390ml-1654161260?w=960&type=s',
+'Hương vị được ưa chuộng tại hơn 190 quôc gia và lọt top những nước giải khát được yêu thích nhất toàn cầu. Với vị chanh tươi mát cùng những bọt ga sảng khoái tê đầu lưỡi giúp bạn đập tan cơn khát ngay tức thì. Sản phẩm cam kết chính hãng, chất lượng và an toàn từ nhà Sprite',
+8000, 15
+);
+
+insert into warehouse (quantity, product_id) VALUES (100,33);
+insert into warehouse (quantity, product_id) VALUES (100,34);
+insert into warehouse (quantity, product_id) VALUES (100,35);
+insert into warehouse (quantity, product_id) VALUES (100,36);
+insert into warehouse (quantity, product_id) VALUES (100,37);
+insert into warehouse (quantity, product_id) VALUES (100,38);
+insert into warehouse (quantity, product_id) VALUES (100,39);
+insert into warehouse (quantity, product_id) VALUES (100,40);
+insert into warehouse (quantity, product_id) VALUES (100,41);
+insert into warehouse (quantity, product_id) VALUES (100,42);
+insert into warehouse (quantity, product_id) VALUES (100,43);
+insert into warehouse (quantity, product_id) VALUES (100,44);
