24b6edc743621c3a247755bff39e12ac4b574482
DOCHIEMDUONG <chiemduong.dp.cntt@gmail.com>
2022-07-15 16:07:49 +0700

    update

---
+++ config/snack/cac_loai_nack.sql
@@ -0,0 +1,178 @@
+insert into `products` ( `name`, `avatar`,`description`,`price`,`category_id`) values 
+
+( 
+'Bánh Que Mix Hương Vị Rong Biển 60g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-banh-que-mix-huong-vi-rong-bien-60g-1655798540?w=960&type=s',
+'Bánh Que Mix Hương Vị Rong Biển 60g là món ăn vặt lí tưởng với những que nhỏ được tẩm ướp gia vị đậm đà, vị rong biển kích thích cảm giác ngon miệng thích hợp cho mọi lứa tuổi đặc biệt là các bạn nhỏ
+Thiết kế túi zip tiện lợi có thể mang theo đi picnic',
+16000, 24
+),
+( 
+'Bánh Que Mix Vị Ớt Cay 60g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-banh-que-mix-vi-ot-cay-60g-1655798473?w=960&type=s',
+'Bánh que Mix vị ớt cay gói 60g của thương hiệu Mix là bánh que có nguồn gốc nội địa Thái Lan với hương vị ớt cay cay tạo cảm giác kích thích khi ăn. Hơn thế nữa bánh que còn giòn tan trong miệng khi ăn. Thích hợp cho ăn vặt gia đình hay văn phòng.',
+16000, 24
+),
+( 
+'Bánh Snack Vị Phô Mai Nabati Richeese Siip Bite 40g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-banh-snack-vi-pho-mai-nabati-richeese-siip-bite-40g-1655798496?w=960&type=s',
+'Siip là một món ăn nhẹ được làm từ ngô giòn có dạng hình trụ',
+7000, 24
+),
+( 
+'Hộp Bánh Que Mix Hương Vị Truyền Thống 150g - 1 Hộp',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-hop-banh-que-mix-huong-vi-truyen-thong-150g-1655798513?w=960&type=s',
+'Tên sản phẩm: Bánh Que MIX Hương Vị Truyền Thống hộp 150g
+Khối lượng tịnh: 150G
+Xuất xứ: Thái Lan
+Thành phần: Bột mì, bột sắn, nước mắm, dầu cọ, chất tạo xốp, chất điều vị, hương liệu tự nhiên,..
+Hướng dẫn sử dụng: Dùng trực tiếp ngay sau khi mở bao bì
+Hướng dẫn bảo quản: Bảo quản ở nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp',
+40000, 24
+),
+( 
+'Oishi Snack Phồng Cá 75g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-oishi-snack-phong-ca-75g-1655700304?w=960&type=s',
+'Snack hải sản mang đậm hương vị biển cả sẽ làm thỏa mãn vị giác của bạn với hình dáng của những chiếc bánh phồng tôm quen thuộc mà còn mới hơn với sự kết hợp hương vị cá',
+12000, 24
+),
+( 
+'Oishi Snack Vị Phồng Mực Indo 75g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-oishi-snack-vi-phong-muc-indo-75g-1656402202?w=960&type=s',
+'Snack hải sản mang đậm hương vị biển cả sẽ làm thỏa mãn vị giác của bạn với hình dáng của những chiếc bánh phồng tôm quen thuộc mà còn mới hơn với sự kết hợp hương vị của mực',
+12000, 24
+),
+( 
+'Oishi Snack Vị Tôm Cay Đặc Biệt 75g - 1 Bịch',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-oishi-snack-vi-tom-cay-dac-biet-75g-1656402214?w=960&type=s',
+'Vô tư ăn liền với Oishi Snack Tôm Cay Đặc Biệt. Sự kết hợp vị tôm tươi ngon và gia vị siêu cay đặc trưng. Qua công nghệ rang không chiên đặc biệt cho trải nghiệm vô tư ăn liền.',
+12000, 24
+),
+( 
+'OStar Khoai Tây Vị Kim Chi 63g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-ostar-khoai-tay-vi-kim-chi-63g-1656402228?w=960&type=s',
+'Snack khoai tây giòn thơm ngon với độ dày vừa phải, khi ăn cảm giác tan trong miệng thích thú. Snack khoai tây vị kim chi Hàn Quốc ostar gói 63g với hương vị kim chi Hàn Quốc lạ miệng thơm ngon, tiện lợi thích hợp đọc sách, xem phim và ăn. Snack OStar là thương hiệu Hàn Quốc',
+14000, 24
+),
+( 
+'OStar Khoai Tây Vị Tảo Biển 63g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-ostar-khoai-tay-vi-tao-bien-63g-1656402244?w=960&type=s',
+'Snack khoai tây giòn thơm ngon với độ dày vừa phải, khi ăn cảm giác tan trong miệng thích thú. Snack khoai tây vị tảo biển ostar  với hương vị tảo biển miệng thơm ngon, tiện lợi thích hợp đọc sách, xem phim và ăn. Snack OStar là thương hiệu Hàn Quốc',
+14000, 24
+),
+( 
+'Snack Bắp Karamucho Vị Cay Đặc Biệt 64g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-karamucho-vi-cay-dac-biet-64g-1654745317?w=960&type=s',
+'Bột bắp 58,9 %, dầu thực vật, bột gia vị ớt 8,8 % (đường, bột tỏi, bột nước tương, chất điều vị (E621, E635), protein thực vật thủy phân, bột hành, chất điều chỉnh độ axit (E330), màu tự nhiên: Parika oleoresin (E160c), chất tạo ngọt nhân tạo: Sucralose (E955)).
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với đậu nành, lúa mì.',
+11000, 24
+),
+( 
+'Snack Bắp Koimucho Vị Bơ Mật Ong 64 G - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-koimucho-vi-bo-mat-ong-64-g-1656402254?w=960&type=s',
+'Thành phần:
+Bột bắp 58,9%, dầu thực vật, bột gia vị bơ mật ong 8,8 % (đường, đường dextrose, maltodextrin, muối, bột kem béo thực vật, bột phô mai, bột sữa nguyên kem, bột whey, chất điều vị (621, 635, 951, 955), chất chống đông vón (551), bột chiết xuất nấm men, hương bơ giống tự nhiên, bột mật ong (1,5 %), đạm thực vật thủy phân, hương phô mai bơ tổng hợp, chất điều chỉnh độ acid (330), hương khói tự nhiên.
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với sữa, gluten, đậu nành',
+11000, 24
+),
+( 
+'Snack Bắp Koimucho Vị Bơ Tỏi 64g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-koimucho-vi-bo-toi-64g-1656402266?w=960&type=s',
+'Thành phần:
+Bột bắp 58,9 %, dầu thực vật, bột gia vị bơ tỏi 8,8 % (đường, kem béo, chất điều vị (E621), muối, maltodextrin, bột tỏi 5,0 %, hương tỏi tự nhiên 4,0 %, màu tự nhiên: Caroten tự nhiên (chiết xuất từ thực vật) (E160a(ii)), hương bơ nhân tạo 0,3 %, chất điều chỉnh độ axit (E330), chất tạo ngọt tự nhiên: Steviol glycosid (E960)).
+<Chú ý>: Sản phẩm có thể chứa nguyên liệu từ lúa mì, tôm, đậu nành, đậu phộng và sữa.',
+11000, 24
+),
+( 
+'Snack Bắp Koimucho Vị Sữa Bắp 64gr - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-koimucho-vi-sua-bap-64gr-1656402277?w=960&type=s',
+'Bột bắp 58,9 %, dầu thực vật, bột gia vị sữa bắp 8,8 % (đường, maltodextrin, muối, bột hương bắp giống tự nhiên 9,0 %, chất điều vị (E621, E627, E631), kem béo, bột sữa nguyên kem 1,5 %, chất tạo ngọt nhân tạo: Aspartam (E951)).
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với sữa.',
+11000, 24
+),
+( 
+'Snack Bắp Koimucho Vị Thịt Nướng 64gr - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-koimucho-vi-thit-nuong-64gr-1656402288?w=960&type=s',
+'Thành phần:
+Bột bắp 58,9 %, dầu thực vật, bột gia vị thịt nướng 8,8 % (đường, maltodextrin, chất điều vị (E621), muối, bột kem béo thực vật, đạm thực vật thủy phân, bột chiết xuất tự nhiên từ thịt bò 1,6 %, hương bò nhân tạo, màu tự nhiên: Parika oleoresin (E160c), bột hương thịt nướng giống tự nhiên 0,4 %, chất điều chỉnh độ axit (E330), màu nhân tạo: Caramen nhóm IV (xử lý amoni sulfit) (E150d), chất tạo ngọt nhân tạo: Aspartam (E951)).
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với sữa, đậu nành.',
+11000, 24
+),
+( 
+'Snack Bắp Vị Cay Ngọt Happy Tos 140g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-vi-cay-ngot-happy-tos-140g-1656402306?w=960&type=s',
+'- Thương hiệu: Happy Tos
+- Vị: Cay ngọt
+- Trọng lượng: 140g
+- Thành phần: Khoai tây miếng (44%), dầu ăn, gia vị hỗn hợp (đường, hương kim chi nhân tạo, muối, maltodextrin, chất chống đông vón, bột nước mắm, gia vị cay, chất điều vị, chiết xuất nấm men, bột giấm, chất điều chỉnh độ axit, chất tạo ngọt tự nhiên), bột bắp, chất kết dính, chất nhũ hóa, chất điều chỉnh độ axit, màu thực phẩm tự nhiên.
+- Được đóng gói trong bao bì chứa đựng thực phẩm đảm bảo An toàn thực phẩm theo quy định của Bộ Y Tế.
+- Bảo quản : Bảo quản nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp',
+29000, 24
+),
+(
+'Snack Bắp Vị Phô Mai Happy Tos 140g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-vi-pho-mai-happy-tos-140g-1656402320?w=960&type=s',
+'- Thương hiệu: Happy Tos
+- Vị: Phomai nacho
+- Trọng lượng: 140g
+- Thành phần: Khoai tây miếng (44%), dầu ăn, gia vị hỗn hợp (đường, hương kim chi nhân tạo, muối, maltodextrin, chất chống đông vón, bột nước mắm, gia vị cay, chất điều vị, chiết xuất nấm men, bột giấm, chất điều chỉnh độ axit, chất tạo ngọt tự nhiên), bột bắp, chất kết dính, chất nhũ hóa, chất điều chỉnh độ axit, màu thực phẩm tự nhiên.
+- Được đóng gói trong bao bì chứa đựng thực phẩm đảm bảo An toàn thực phẩm theo quy định của Bộ Y Tế.
+- Bảo quản : Bảo quản nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp',
+29000,24
+),
+(
+'Snack Bắp Vị Truyền Thống Happy Tos 160g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-bap-vi-truyen-thong-happy-tos-160g-1656402334?w=960&type=s',
+'- Thương hiệu: Happy Tos
+- Vị: Truyền thống
+- Trọng lượng: 160g
+- Thành phần: Khoai tây miếng (44%), dầu ăn, gia vị hỗn hợp (đường, hương kim chi nhân tạo, muối, maltodextrin, chất chống đông vón, bột nước mắm, gia vị cay, chất điều vị, chiết xuất nấm men, bột giấm, chất điều chỉnh độ axit, chất tạo ngọt tự nhiên), bột bắp, chất kết dính, chất nhũ hóa, chất điều chỉnh độ axit, màu thực phẩm tự nhiên.
+- Được đóng gói trong bao bì chứa đựng thực phẩm đảm bảo An toàn thực phẩm theo quy định của Bộ Y Tế.
+- Bảo quản : Bảo quản nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp',
+29000,24
+),
+(
+'Snack Cua Xanh Kinh Đô 29gr - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-cua-xanh-kinh-do-29gr-1656402351?w=960&type=s',
+'Snack cua xanh không chứa hóa chất, chất bảo quản độc hại, đặc biệt không chứa gluten, đảm bảo an toàn cho sức khỏe người tiêu dùng. Snack Cua Xanh Kinh Đô 29gr có hương vị thơm ngon của những nguyên liệu tự nhiên được chọn lọc kỹ càng, sẽ mang lại cho bạn những phút giải trí và thưởng thức thật tuyệt vời bên cạnh bạn bè hoặc người thân.
+Snack Cua Xanh Kinh Đô 29gr được đóng gói tiện dụng, dễ bảo quản và dùng được lâu. Bạn cũng có thể mang theo dễ dàng trong những chuyến đi chơi, picnic để cùng chia sẻ với mọi người hay dành làm quà tặng cho bạn bè, người thân.',
+8000,24
+),
+(
+'Snack Khoai Tây Karamucho Strong Lát Dày Vị Cay Đặc Biệt 80gr - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-khoai-tay-karamucho-strong-lat-day-vi-cay-dac-biet-80gr-1656402368?w=960&type=s',
+'Thành phần:
+Khoai tây 59,0 %, dầu thực vật, bột gia vị ớt 6,0 % (đường, bột rau, muối, chất điều vị (E621, E627, E508, E631), maltodextrin, bột nước tương, gia vị và thảo mộc, chất điều chỉnh độ axit (E330, E331(iii), E300), màu nhân tạo: Caramen nhóm III (xử lý amoni) (E150c), chất tạo ngọt nhân tạo: Aspartam (E951)).
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với gluten, đậu nành, sữa.',
+25000,24
+),
+(
+'Snack Khoai Tây Karamucho Strong Lát Dày Vị Thịt Nướng Tokyo 80g - 1 Gói',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-snack-khoai-tay-karamucho-strong-lat-day-vi-thit-nuong-tokyo-80g-1656402412?w=960&type=s',
+'Thành phần:
+Khoai tây 59,0 %, dầu thực vật, bột gia vị thịt nướng 6,0 % (đường, maltodextrin, chất điều vị (E621), muối, bột kem béo thực vật, bột tiêu đen, đạm thực vật thủy phân, bột gà, hương gà giống tự nhiên, hương tỏi giống tự nhiên, màu nhân tạo: Caramen nhóm IV (xử lý amoni sulfit) (E150d), hương bò giống tự nhiên, màu tự nhiên: Parika oleoresin (E160c), chất điều chỉnh độ axit (E330), chất tạo ngọt nhân tạo: Aspartam (E951)).
+<Chú ý>: Sản phẩm không phù hợp cho người dị ứng với sữa, đậu nành',
+25000,24
+);
+
+insert into warehouse (quantity, product_id) VALUES (100,76);
+insert into warehouse (quantity, product_id) VALUES (100,77);
+insert into warehouse (quantity, product_id) VALUES (100,78);
+insert into warehouse (quantity, product_id) VALUES (100,79);
+insert into warehouse (quantity, product_id) VALUES (100,80);
+insert into warehouse (quantity, product_id) VALUES (100,81);
+insert into warehouse (quantity, product_id) VALUES (100,82);
+insert into warehouse (quantity, product_id) VALUES (100,83);
+insert into warehouse (quantity, product_id) VALUES (100,84);
+insert into warehouse (quantity, product_id) VALUES (100,85);
+insert into warehouse (quantity, product_id) VALUES (100,86);
+insert into warehouse (quantity, product_id) VALUES (100,87);
+insert into warehouse (quantity, product_id) VALUES (100,88);
+insert into warehouse (quantity, product_id) VALUES (100,89);
+insert into warehouse (quantity, product_id) VALUES (100,90);
+insert into warehouse (quantity, product_id) VALUES (100,91);
+insert into warehouse (quantity, product_id) VALUES (100,92);
+insert into warehouse (quantity, product_id) VALUES (100,93);
+insert into warehouse (quantity, product_id) VALUES (100,94);
+insert into warehouse (quantity, product_id) VALUES (100,95);
+insert into warehouse (quantity, product_id) VALUES (100,65);
