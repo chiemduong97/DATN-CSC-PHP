184689d09ef3e54c5e03d75e58d52a64b95ee517
ManhDuc <nguyenmanhduc310520@gmail.com>
2022-08-07 12:25:44 +0700

    u

---
+++ config/nuoc_suoi.sql
@@ -0,0 +1,103 @@
+insert into `products` ( `name`, `avatar`,`description`,`price`,`category_id`) values 
+
+( 
+'Nước Bù Khoáng Aquarius Chai 390ml',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-bu-khoang-aquarius-chai-390ml-1654834769?w=960&type=s',
+'Sản phẩm nước bù khoáng cho thể thao, Aquarius hương vị êm dịu giúp giải tan cơn khát của bạn. Nước uống vận động Aquarius 390ml với thành phần gồm muối, đường mía, Kali và chất Quarana có chức năng trì hoãn cảm giác mệt mỏi, nước uống thể thao giúp duy trì cơ thể hoạt động ở mức ổn định.
+
+Thương hiệu: Aquarius (Việt Nam)
+Sản xuất tại Việt Nam
+Loại sản phẩm: Nước uống vận động
+Dung tích: 390ml
+Hướng dẫn sử dụng: Lắc nhẹ trước khi uống, dùng ngay sau khi mở nắp. Ngon hơn khi uống lạnh.
+Bảo quản: Để nơi khô ráo, thoáng mát, tránh ánh sáng trực tiếp hoặc nơi có nhiệt độ cao.',8000, 15
+),
+( 
+'Nước Khoáng Vivant 5l',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-khoang-vivant-5l-1654160677?w=960&type=s',
+'Nước khoáng Vivant chứa nhiều khoáng chất hòa tan tự nhiên trong nước có tác dụng tốt cho sức khỏe con người:
+-Canxi: Giúp cho xương cứng, răng tốt và cơ bắp khỏe mạnh.
+-Magiê: Tham gia vào nhiều phản ứng xúc tác và kích thích cơ thần kinh.
+-Kali: Duy trì sự cân bằng độ pH và cần thiết cho các phản ứng hóa học cung cấp năng lượng bên trong cơ thể.
+-Natri: Duy trì sự cân bằng lượng chất lỏng của cơ thể và giúp cho sự vận động của hệ thần kinh.
+-Acid Silic: Giúp cho hệ thần kinh vận động tốt hơn.
+-Bicarbonat: Giảm độ acid trong dạ dày.
+Sản phẩm được sản xuất trên dây chuyền sản xuất tự động và tiên tiến nhất từ Mỹ, Ba Lan mang lại cho nhà tiêu dùng sản phẩm chất lượng tốt nhất.',
+29000, 15
+),
+( 
+'Nước Tinh Khiết Number 1 500ml',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-tinh-khiet-number-1-500ml-1655893561?w=960&type=s',
+'Sản phẩm nước uống chất lượng, chính hãng thương hiệu Number 1 được sản xuất với công nghệ hiện đại giúp giữ được hương vị tự nhiên của nước mà không hề chứa bất kì thành phần hóa học nào khác. Nước tinh khiết giúp loại bỏ các chất thừa cũng như độc tố ra khỏi cơ thể nếu uống đủ nước.',
+4000, 15
+),
+( 
+'Nước Uống Ion Kiềm Saka 1.2l',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-uong-ion-kiem-saka-1-2l-1654161833?w=960&type=s',
+'TÍNH KIỀM
+pH của nước Saka từ 7.5 - 9.5 giúp cân bằng môi trường pH cho máu và các tế bào, trung hoà lượng axit dư thừa trong cơ thể, một trong những nguyên nhân gây ra bệnh tật
+CHỐNG OXY HÓA
+Nước Saka có tính khử oxy hoá cao giúp trung hoà được các gốc tự do, ngăn ngừa sự lão hoá (Độ chống oxy hóa 200 đến 800)
+BỔ SUNG LƯỢNG KHOÁNG
+CẦN THIẾT
+Vị ngọt tự nhiên các ion Na+  , Ca2+    , K+  , Mg2+
+được bổ sung giúp cơ thể ở trạng thái cân bằng và khoẻ mạnh
+CẤU TRÚC PHÂN TỬ NƯỚC NHỎ
+Giúp tăng độ thẩm thấu đến từng tế bào và tăng cường thanh lọc cơ thể
+KHÔNG DÙNG HOÁ CHẤT TĂNG ĐỘ PH
+Saka được sản xuất hoàn toàn dựa trên quá trình điện phân nước để tạo ra nước ion kiềm, một loại nước tốt cho sức khoẻ
+PHÒNG & HỖ TRỢ BỆNH
+Tiểu đường, béo phì, các bệnh về đường ruột như khó tiêu, ợ chua hay gout...',15000, 15
+),
+( 
+'Pocari Sweat Lon 330ml',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-pocari-sweat-lon-330ml-1653720643?w=960&type=s',
+'Nước bù khoáng Pocari Sweat giúp cơ thể bổ sung nước và các ion thiết yếu như Na+, K+, Mg2+, Cl-, Ca2+, tái tạo năng lượng và sức sống ngay từ bên trong. Nước khoáng i-on Pocari Sweat 330ml được sản xuất từ nước khoáng trong thiên nhiên an toàn và tốt cho sức khỏe.
+
+Thương hiệu: Pocari Sweat (Nhật Bản)
+Sản xuất tại: Việt Nam
+Thể tích: 330ml
+Hướng dẫn sử dụng: Lắc nhẹ trước khi uống, dùng ngay sau khi mở nắp. Ngon hơn khi uống lạnh.
+Bảo quản: Để nơi khô ráo, thoáng mát, tránh ánh sáng trực tiếp hoặc nơi có nhiệt độ cao.',
+15000, 15
+),
+( 
+'Pocari Sweat Chai 900ml ',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-pocari-sweat-chai-900ml-1652087052?w=960&type=s',
+'BÙ ĐẮP NƯỚC, ION VÀ CHẤT ĐIỆN GIẢI
+POCARI là thức uống tốt cho sức khỏe, không gaz, không caffeine, không đường hóa học, không chất tạo màu và chất bảo quản. Với thành phần tương tự như lượng nước mất từ cơ thể, POCARI bổ sung NƯỚC và các ION thiết yếu như Na+, K+, Mg2+, Cl–, Ca2+, giúp cơ thể hấp thụ nhanh, tái tạo năng lượng và sức sống ngay từ bên trong.
+THỨC UỐNG TỐT CHO SỨC KHỎE ĐẾN TỪ NHẬT BẢN
+POCARI là sản phẩm của Tập đoàn Dược hàng đầu Nhật Bản, Otsuka. POCARI được 90% bác sĩ Nhật Bản khuyên dùng.
+GIÚP ÍCH CHO CÁC TRƯỜNG HỢP MẤT NƯỚC
+Có thể uống Pocari mọi lúc mọi nơi.',
+27000, 15
+),
+( 
+'Pocari Sweat 350ml',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-pocari-sweat-lon-330ml-1653720643?w=960&type=s',
+'Thông tin sản phẩm
+Nước bù khoáng Pocari Sweat bổ sung nước và các ion thiết yếu như Na+, K+, Mg2+, Cl-, Ca2+, giúp cơ thể hấp thụ nhanh, tái tạo năng lượng và sức sống ngay từ bên trong. Nước khoáng i-on Pocari Sweat 350ml được chiết xuất từ nguồn nước khoáng trong thiên nhiên tốt cho sức khỏe
+Thương hiệu Pocari Sweat (Nhật Bản)
+Sản xuất tại Việt Nam
+Thể tích 350ml
+Hướng dẫn sử dụng Lắc nhẹ trước khi uống, dùng ngay sau khi mở nắp. Ngon hơn khi uống lạnh.
+Bảo quản Để nơi khô ráo, thoáng mát, tránh ánh sáng trực tiếp hoặc nơi có nhiệt độ cao.',
+13000, 15
+),
+( 
+
+
+
+
+insert into warehouse (quantity, product_id) VALUES (100,33);
+insert into warehouse (quantity, product_id) VALUES (100,34);
+insert into warehouse (quantity, product_id) VALUES (100,35);
+insert into warehouse (quantity, product_id) VALUES (100,36);
+insert into warehouse (quantity, product_id) VALUES (100,37);
+insert into warehouse (quantity, product_id) VALUES (100,38);
+insert into warehouse (quantity, product_id) VALUES (100,39);
+insert into warehouse (quantity, product_id) VALUES (100,40);
+insert into warehouse (quantity, product_id) VALUES (100,41);
+insert into warehouse (quantity, product_id) VALUES (100,42);
+insert into warehouse (quantity, product_id) VALUES (100,43);
+insert into warehouse (quantity, product_id) VALUES (100,44);
---
+++ config/ruou.sql
@@ -0,0 +1,94 @@
+insert into `products` ( `name`, `avatar`,`description`,`price`,`category_id`) values 
+
+( 
+'[1 Tặng 1] - Nước Trái Cây Xoài Tự Nhiên Th True Juice 350ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-1-tang-1-nuoc-trai-cay-xoai-tu-nhien-th-true-juice-350ml-1657447567?w=960&type=s',
+'Nước ép trái cây TH True Juice chắc lọc vị ngon ngọt tự nhiên của các loại trái cây, hoàn toàn từ thiên nhiên. Nước Trái Cây Xoài Tự Nhiên Th True Juice 350ml lưu giữ các dưỡng chất và hương vị đặc trưng, thơm ngon của xoài. Nước ép xoài không chỉ giúp giải khát mà còn mang lại sức khoẻ tốt khi dùng.',
+25000, 16
+),
+( 
+'Nước Ngọt Sprite Pet 1,85l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-sprite-pet-1-85l-1654674645?w=960&type=s',
+'Hương vị được ưa chuộng tại hơn 190 quôc gia và lọt top những nước giải khát được yêu thích nhất toàn cầu. Với vị chanh tươi mát cùng những bọt ga sảng khoái tê đầu lưỡi giúp bạn đập tan cơn khát ngay tức thì. Sản phẩm cam kết chính hãng, chất lượng và an toàn từ nhà Sprite
+Thương hiệu: Sprite (Việt Nam)',
+21000, 16
+),
+( 
+'Sá Xị Chương Dương Chai 1.5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-sa-xi-chuong-duong-chai-1-5l-1656646633?w=960&type=s',
+'Sá xị là sản phẩm nước giải khát truyền thống của Chương Dương và là sản phẩm mang hương vị độc đáo của Việt Nam, được khách hàng rất ưa chuộng. Sản phẩm có thành phần quế và hồi hỗ trợ tiêu hóa, hô hấp, tuần hoàn, làm ấm cơ thể.
+Ngon hơn khi uống lạnh',
+23000, 16
+),
+( 
+'Coca Zero 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-coca-zero-390ml-1654184890?w=960&type=s',
+'Từ thương hiệu nước giải khát Coca Cola nổi tiếng thế giới được ưa chuộng tại nhiều nhiều quốc gia. Coca Zero 390ml cho cơ thể cảm giác nhẹ nhàng, ăn ngon hơn, không đường không calo phù hợp cho những ai yêu thích nước ngọt nhưng vẫn muốn giữ thói quen ăn uống lành mạnh
+Thương hiệu: Coca Cola (Mỹ)',
+9000, 16
+),
+( 
+'Nước Ngọt Coca Ít Đường 1,5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-it-duong-1-5l-1654681363?w=960&type=s',
+'Không chỉ giúp giải tỏa cơn khát mà còn cung cấp nguồn năng lượng cùng hàm lượng khoáng chất dồi dào, cho bạn khơi lại hứng khởi ngay từ ngụm đầu tiên.Nước Ngọt Coca Ít Đường 1,5l phù hợp cho những ai thích nước ngọt và lối sống ít đường, lành mạnh. Cam kết chính hãng từ Coca Cola
+Thương hiệu: Coca Cola (Mỹ)',
+22000, 16
+),
+( 
+'Nước Ngọt Coca Ít Đường 2,25l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-it-duong-2-25l-1654681379?w=960&type=s',
+'Từ thương hiệu loại nước giải khát Coca Cola được nhiều người yêu thích với hương vị thơm ngon, sảng khoái. Nước ngọt Coca Cola Ít Đường chai 2.25 lít với lượng gas lớn sẽ giúp bạn xua tan mọi cảm giác mệt mỏi, căng thẳng, đem lại cảm giác thoải mái sau khi hoạt động ngoài trời.',
+28000, 16
+),
+( 
+'Nước Ngọt Coca Không Đường 1,5l - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-coca-khong-duong-1-5l-1654184857?w=960&type=s',
+'Từ thương hiệu nước giải khát Coca Cola nổi tiếng thế giới được ưa chuộng tại nhiều nhiều quốc gia. Nước Ngọt Coca Không Đường 1,5l cho cơ thể cảm giác nhẹ nhàng, ăn ngon hơn, không đường không calo phù hợp cho những ai yêu thích nước ngọt nhưng vẫn muốn giữ thói quen ăn uống lành mạnh
+Thương hiệu: Coca Cola (Mỹ)',
+20000, 16
+),
+( 
+'Nước Ngọt Fanta Cam Chai 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-fanta-cam-chai-390ml-1654185077?w=960&type=s',
+'Là sản phẩm nước ngọt có gas của thương hiệu Fanta nổi tiếng giúp giải khát sau khi hoạt động ngoài trời, giải tỏa căng thẳng, mệt mỏi khi học tập, làm việc. Nước Ngọt Fanta Cam Chai 390ml thơm ngon kích thích vị giác, chứa nhiều vitamin C sẽ cung cấp năng lượng cho cơ thể khỏe mạnh',
+8000, 16
+),
+(
+'Nước Ngọt Mirinda Cam 1.5 Lít - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-mirinda-cam-1-5-lit-1654161021?w=960&type=s',
+'Sản phẩm nước ngọt giải khát từ thương hiệu Mirinda nổi tiếng được nhiều người ưa chuộng. Nước ngọt Mirinda cam 1.5 lít với hương vị cam đặc trưng, không chỉ giải khát, mà còn bổ sung thêm vitamin C giúp lấy lại năng lượng cho hoạt động hàng ngày. Cam kết chính hãng và an toàn
+Hướng dẫn sử dụng: Dùng trực tiếp, ngon hơn khi uống lạnh.
+Bảo quản: Để nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp.',
+22000, 16
+),
+(
+'Nước Ngọt Mirinda Xá Xị 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-mirinda-xa-xi-390ml-1653557418?w=960&type=s',
+'Sản phẩm nước ngọt giải khát từ thương hiệu Mirinda nổi tiếng được nhiều người ưa chuộng với hương và vị hấp dẫn. Nước ngọt Mirinda hương xá xị chai 390ml có hương xá xị tự nhiên, độc đáo giúp bạn giải nhanh cơn khát,  với vị gas nhẹ là thức uống giải khát tuyệt vời dành cho mọi lứa tuổi',
+8000, 16
+),
+(
+'Nước Ngọt Pepsi 1.5 Lít - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-pepsi-1-5-lit-1653557377?w=960&type=s',
+'Là loại nước ngọt được nhiều người yêu thích với hương vị thơm ngon, sảng khoái. Nước ngot Pepsi 1.5 lít với lượng gas lớn sẽ giúp bạn xua tan mọi cảm giác mệt mỏi, căng thẳng, đem lại cảm giác thoải mái sau khi hoạt động ngoài trời.',
+21000, 16
+),
+(
+'Nước Ngọt Sprite Hương Chanh 390ml - 1 Chai',
+'https://tea-3.lozi.vn/v1/ship/resized/product-avatar-nuoc-ngot-sprite-huong-chanh-390ml-1654161260?w=960&type=s',
+'Hương vị được ưa chuộng tại hơn 190 quôc gia và lọt top những nước giải khát được yêu thích nhất toàn cầu. Với vị chanh tươi mát cùng những bọt ga sảng khoái tê đầu lưỡi giúp bạn đập tan cơn khát ngay tức thì. Sản phẩm cam kết chính hãng, chất lượng và an toàn từ nhà Sprite',
+8000, 16
+);
+
+insert into warehouse (quantity, product_id) VALUES (100,33);
+insert into warehouse (quantity, product_id) VALUES (100,34);
+insert into warehouse (quantity, product_id) VALUES (100,35);
+insert into warehouse (quantity, product_id) VALUES (100,36);
+insert into warehouse (quantity, product_id) VALUES (100,37);
+insert into warehouse (quantity, product_id) VALUES (100,38);
+insert into warehouse (quantity, product_id) VALUES (100,39);
+insert into warehouse (quantity, product_id) VALUES (100,40);
+insert into warehouse (quantity, product_id) VALUES (100,41);
+insert into warehouse (quantity, product_id) VALUES (100,42);
+insert into warehouse (quantity, product_id) VALUES (100,43);
+insert into warehouse (quantity, product_id) VALUES (100,44);
