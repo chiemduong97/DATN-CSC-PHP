1b0fe738a17e578f0b3810418a56f046fee3d623
DOCHIEMDUONG <chiemduong.dp.cntt@gmail.com>
2022-08-28 09:09:39 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
 	class DatabaseConfig{
-		// private $host = "localhost";
-		private $host = "sql6.freemysqlhosting.net";
-		// private $username = "root";
-		private $username = "sql6515473";
-		// private $password = "";
-		private $password = "wzz3w3lVBX";
-		// private $db_name = "datn_csc";
-		private $db_name = "sql6515473";
+		private $host = "localhost";
+		// private $host = "sql6.freemysqlhosting.net";
+		private $username = "root";
+		// private $username = "sql6515473";
+		private $password = "";
+		// private $password = "wzz3w3lVBX";
+		private $db_name = "datn_csc";
+		// private $db_name = "sql6515473";
 		public $connect; 
 		public function db_connect(){
 			$this -> connect = null;
